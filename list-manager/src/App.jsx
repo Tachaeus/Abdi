9d6181b0331b39ab5e04aa3b8383528c75bb8ade
Abdallah Abdul <tachincursion@gmail.com>
2025-04-05 18:40:44 +0000

    List_Manager

---
+++ list-manager/src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react"
+import "./App.css"
+
+function App() {
+  const [items, setItems] = useState([]);
+
+  const [listItem, setListItem] = useState("");
+
+  const addListItem = (i) => {
+    setListItem(i.target.value);
+  }
+
+  const addItem = () => {
+    if (listItem.trim() === "") {
+      return;
+    }
+    setItems([...items, listItem]);
+    setListItem("");
+  }
+
+  return (
+    <div className="App">
+      <h3 className="heading">Dynamic List Manager</h3>
+      <div className="entry">
+      <input type="text" value={listItem} onChange={addListItem} placeholder="Enter an item" /><button onClick={addItem}>Add Item</button>
+      </div>
+      <ul>
+        {items.map((item) => (<li key={item}>{item}</li>))
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default App;
